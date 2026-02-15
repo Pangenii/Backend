caf0411f558cfdb438bcb1fd9adcfb9958ed8f82
Bishwa Pangeni <erbishwa01@gmail.com>
2026-02-15 18:44:20 +0545

    [main] Global rate limit and Endpoints based rate limiting added

--- auth-service/src/server.js
+++ auth-service/src/server.js
@@ -5,8 +5,14 @@ import { connectToDB } from '../config/connectDB.js';
 import helmet from "helmet";
 import { RateLimiterRedis } from "rate-limiter-flexible";
 import Redis from "ioredis";
+import { rateLimit } from 'express-rate-limit'
+import { RedisStore } from 'rate-limit-redis'
+import { authrouter } from './routes/auth-service.js';
+import { errorHandler } from './middleware/errorHandler.js';
 
 const app = express();
+const PORT = process.env.PORT;
+
 connectToDB(process.env.MONGO_URI);
 const redisClient = new Redis(process.env.REDIS_URL);
 //middleware
@@ -21,3 +27,61 @@ app.use((req, res, next) => {
 })
 
 //DDOS PROTECTION
+const rateLimiter = new RateLimiterRedis({
+    storeClient: redisClient,
+    keyPrefix: "middleware",
+    points: 10, // 10 requests
+    duration: 1, // per 1 second by IP
+})
+
+const rateLimiterMiddleware = (req, res, next) => {
+    rateLimiter.consume(req.ip)
+        .then(() => {
+            next();
+        })
+        .catch(() => {
+            logger.warn("REQUEST LIMIT EXCEEDED")
+            res.status(429).json({
+                success: false,
+                message: "REQUEST LIMIT EXCEEDED"
+            });
+        });
+};
+
+app.use(rateLimiterMiddleware);
+
+//Endpoints based rate limiting
+const limiterEndpoints = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100,
+    standardHeaders: true,
+    legacyHeaders: false,
+    handler: (req, res) => {
+        logger.warn(`REQUEST LIMIT EXCEEDED FOR IP : ${req.ip}`);
+        res.status(429).json({
+            success: false,
+            message: "REQUEST LIMIT EXCEEDED"
+        });
+    },
+    store: new RedisStore({
+        sendCommand: (...args) => redisClient.call(...args)
+    })
+
+})
+
+//Implementing endpoints based rate limiting
+app.use("/api/auth/register", limiterEndpoints);
+
+//routes
+app.use("/api/auth", authrouter)
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+    logger.info(`Identity Service is running on PORT : ${PORT}`)
+})
+
+//unhandled 
+process.on("unhandledRejection", (reason, promise) => {
+    logger.error("Unhandled Rejection at : ", promise, " reason : ", reason);
+})
